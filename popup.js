37f6780c3413d5a55f90ab9fe607d0c59e3cc2bf
youguifang <guifang.you@tcl.com>
2021-06-29 09:50:40 +0800

    一键添加全部打开的tab

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 const addBtn = document.getElementById("addBtn")
 const viewBtn = document.getElementById("viewBtn")
+const addAll = document.getElementById("addAll")
 let isAdding = false; // 防止多次重复添加
+let isAddingAll = false;
 /**
  * 如果已在列表中，则不展示添加按钮
  */
@@ -25,12 +27,16 @@ addBtn.addEventListener("click", async () => {
       console.log("get tabs", tabs)
       if (tabs) {
         const tabList = [...tabs]
-        tabList.unshift(tab)
+        tabList.unshift({
+          url: tab.url,
+          title: tab.title
+        })
         chrome.storage.sync.set({ "tabs": tabList });
       } else {
         chrome.storage.sync.set({ "tabs": [tab] });
       }
       addBtn.innerText = "已添加√"
+      isAdding = false
       setTimeout(() => {
         addBtn.style.display = "none"
       }, 1500)
@@ -40,4 +46,33 @@ addBtn.addEventListener("click", async () => {
 
 viewBtn.addEventListener("click", async () => {
   window.open(chrome.extension.getURL('list.html'));
+});
+
+addAll.addEventListener("click", async () => {
+  if (isAddingAll) return;
+  isAddingAll = true;
+  chrome.storage.sync.get("tabs", ({ tabs }) => {
+    console.log("get tabs", tabs)
+    let tabList = []
+    if (tabs) {
+      tabList = [...tabs]
+    }
+    chrome.windows.getAll({ populate: true }, function (windows) {
+      windows.forEach(function (window) {
+        window.tabs.forEach(function (tab) {
+          console.log(111, tab.url)
+          tabList.unshift({
+            url: tab.url,
+            title: tab.title
+          });
+        });
+      });
+      chrome.storage.sync.set({ "tabs": tabList });
+      isAddingAll = false;
+      addAll.innerText = "添加成功√"
+      setTimeout(() => {
+        addAll.innerText = "加入所有打开tab"
+      }, 5000)
+    });
+  });
 });
